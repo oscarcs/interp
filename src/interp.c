63aef2eda44ee427e1d7a816b0a6b8745e2a41b2
oscarcs <nxtrafalgar@yahoo.co.nz>
2016-09-25 22:17:47 +1300

    C Interpreter: Finish 'assembler', load files via commandline args.

--- src/interp.c
+++ src/interp.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define NIL 9920901
 #define MAX_DEPTH 30
 #define MAX_INSTR 100
-#define MAX_INSTR_LEN 16
+#define MAX_LINE_LEN 100
 #define DEBUG_INSTR 0
 #define DEBUG_STACK 0
+#define DEBUG_ASSEMBLER 0
 
 void dump_stack(void);
 void add_instr(int code, int arg1);
 void add_label(int addr);
 int exec(int code, int arg1);
+void load_asm(char* filename);
 int instr_to_code(char* line);
 int jump(int addr);
 void push(int x);
@@ -27,85 +29,34 @@ int labels[MAX_INSTR] = {NIL};
 int stack[MAX_DEPTH];
 int depth = -1;
 
-int main(void)
+int main(int argc, char **argv)
 {
 	/* 
 	 * TABLE OF INSTRUCTIONS :^)
-	 * 0    |    NOP
-	 * 1	|	PUSH
-	 * 2	|	POP
-	 * 3	|	OUT
-	 * 4	|	ADD
-	 * 5	|	SUB 
-	 * 6	|	MUL
-	 * 7	|	DIV
-	 * 8	|	DUP
-	 * 9    |    AND
-	 * 10   |    OR
-	 * 100  |	JMP	
-	 * 101  |	CMP
-	 * 102  |	JNZ
-	 * 103  |	JZ
+	 * 0   |   NOP
+	 * 1   |   PUSH
+	 * 2   |   POP
+	 * 3   |   OUT
+     * 4   |   COUT
+	 * 5   |   ADD
+	 * 6   |   SUB 
+	 * 7   |   MUL
+	 * 8   |   DIV
+	 * 9   |   DUP
+	 * 10  |   AND
+	 * 11  |   OR
+	 * 100 |   JMP	
+	 * 101 |   CMP
+	 * 102 |   JNZ
+	 * 103 |   JZ
 	 */
-
-	/* 
-	 * ;TEST UNCONDITIONAL JUMP
-	 * NOP
-	 * PUSH 1
-	 * PUSH 1
-	 * PUSH 6
-	 * JMP
-	 * SUB
-	 * ADD
-	 * OUT
-	 */
-	/*
-	add_instr(0, NIL);   
-	add_instr(1, 1);     
-	add_instr(1, 1);     
-	add_instr(1, 6);     
-	add_instr(100, NIL); 
-	add_instr(5, NIL);   
-	add_instr(4, NIL);   
-	add_instr(3, NIL);   
-	*/
-
-	/* 
-	 * ;TEST CONDITIONAL JUMP
-	 * PUSH 0
-	 * PUSH 1
-	 * ADD
-	 * OUT
-	 * DUP
-	 * PUSH 10
-	 * CMP
-	 * PUSH 1
-	 * JMPZ
-	 */
-	/*
-	add_instr(1, 0);
-	add_instr(1, 1);
-	add_instr(4, NIL);
-	add_instr(3, NIL);
-	add_instr(8, NIL);
-	add_instr(1, 10);
-	add_instr(101, NIL);
-	add_instr(1, 1);
-	add_instr(103, NIL);
-    */
-
-    /*
-     * TEST ASSEMBLER
-     */
-    instr_to_code("PUSH 0");
-    instr_to_code("PUSH 1");
-    instr_to_code("ADD");
-    instr_to_code("OUT");
-    instr_to_code("DUP");
-    instr_to_code("PUSH 10");
-    instr_to_code("CMP");
-    instr_to_code("PUSH 1");
-    instr_to_code("JZ");
+    
+    if (argc < 2)
+    {
+        printf("Interp: Provide a filename to load.\n");
+        return 0;
+    }
+    load_asm(argv[1]);
 
 	/* Reset the instruction pointer after loading */
 	ip = -1;
@@ -132,11 +83,17 @@ void dump_stack(void)
 
 void add_instr(int code, int arg1)
 {
-    printf("Added: %i %i\n", code, arg1);
-
-	instructions[ip] = code;
-	args[ip] = arg1;
-	ip++;
+    if (ip < MAX_INSTR)
+    {
+        if (DEBUG_ASSEMBLER) printf("Added: %i %i\n", code, arg1);
+        instructions[ip] = code;
+        args[ip] = arg1;
+        ip++;
+    }
+    else
+    {
+        printf("Error: Max number of instructions exceeded: %i\n", ip);
+    }
 }
 
 void add_label(int addr)
@@ -166,52 +123,57 @@ int exec(int code, int arg1)
         break;
 
         case 3: if (DEBUG_INSTR) printf("OUT \n");
-            /* Print the top value on the stack */
-            printf("%i\n", peek());
+            /* Print the top value on the stack. */
+            printf("%i", peek());
+        break;
+            
+        case 4: if (DEBUG_INSTR) printf("COUT \n");
+            /* Print the top value on the stack as a char. */
+            printf("%c", peek());
         break;
 
-        case 4: if (DEBUG_INSTR) printf("ADD \n");
-            /* Add the top two values and push result */
+        case 5: if (DEBUG_INSTR) printf("ADD \n");
+            /* Add the top two values and push result. */
             a = pop();
             b = pop();
             push(a + b);
         break;
 
-        case 5: if (DEBUG_INSTR) printf("SUB \n");
-        	/* Subtract the second  value from the top value */
+        case 6: if (DEBUG_INSTR) printf("SUB \n");
+        	/* Subtract the second  value from the top value. */
 			a = pop();
 			b = pop();
 			push(a - b);			
         break;
         
-        case 6: if (DEBUG_INSTR) printf("MUL \n");
-            /* Multiply the top two values and push result */
+        case 7: if (DEBUG_INSTR) printf("MUL \n");
+            /* Multiply the top two values and push result. */
             a = pop();
             b = pop();
             push(a * b);
         break;
 
-		case 7: if (DEBUG_INSTR) printf("DIV \n");
-        	/* Divide the top two values and push result */
+		case 8: if (DEBUG_INSTR) printf("DIV \n");
+        	/* Divide the top two values and push result. */
             a = pop();
             b = pop();
             push(a / b);
         break;
 
-        case 8: if (DEBUG_INSTR) printf("DUP \n");
+        case 9: if (DEBUG_INSTR) printf("DUP \n");
         	/* Duplicate the top value on the stack. */
         	a = peek();
         	push(a);
         break;
 
-        case 9: if (DEBUG_INSTR) printf("AND \n");
+        case 10: if (DEBUG_INSTR) printf("AND \n");
         	/* Bitwise AND the top two values. */
             a = pop();
         	b = pop();
         	push(a & b);
         break;
 
-        case 10: if (DEBUG_INSTR) printf("OR \n");
+        case 11: if (DEBUG_INSTR) printf("OR \n");
         	/* Bitwise AND the top two values. */
             a = pop();
         	b = pop();
@@ -258,59 +220,77 @@ int exec(int code, int arg1)
     return 0;
 }
 
+void load_asm(char* filename)
+{
+    FILE *fp;
+    fp = fopen(filename, "r");
+    while(1)
+    {
+        char line[MAX_LINE_LEN];
+        if (fgets(line, 150, fp) == NULL) break;
+        instr_to_code(line);
+    }
+}
+
 int instr_to_code(char* line)
 {
-    int i;
+    int i = 0;
+    int length;
+    char instr[MAX_LINE_LEN] = {0};
 
-    i = 0;
-    while (line[i] != ' ' && line[i] != '\0')
     {
-        i++;
-    }
+        while (line[i] != '\n' && line[i] != '\r' && line[i] != ';' &&
+               line[i] != '\0' && line[i] != ' ')
+        {
+            i++;
+        }
     
-    int length = i;
+        length = i;
 
-    /* Not a valid instruction. */
-    if (length > MAX_INSTR_LEN)
-    {
-        printf("ERROR: Not a valid instruction: %s\n", line);
-    } 
-
-    /* Copy instruction only. */
-    char instr[MAX_INSTR_LEN] = {0}; 
-    for (i = 0; i < length; i++)
-    {
-        instr[i] = line[i];
+        /* Not a valid instruction. */
+        if (length > MAX_LINE_LEN)
+        {
+            printf("ERROR: Not a valid instruction: %s\n", line);
+        } 
+    
+        /* Copy instruction only. */
+        
+        for (i = 0; i < length; i++)
+        {
+            instr[i] = line[i];
+        }
+        instr[length] = '\0';
+        /* printf("instr: %s\n", instr); */
     }
-    instr[length] = '\0';
-    /* printf("instr: %s\n", instr); */
-
 
     int start = length;
     i = start;
-    /* Trim front of string */
-    while(line[i] == ' ')
+    char arg[MAX_LINE_LEN] = {0}; 
+    
     {
-        i++;
-    }
-    start = i;
-    /* Get the argument */
-    while (line[i] != '\n' && line[i] != '\r' && 
-           line[i] != ';'  && line[i] != '\0' && line[i] != ' ')
-    { 
-        /* printf("%c", line[i]); */
-        i++;
-    }
-    length = i;
+        /* Trim front of string */
+        while(line[i] == ' ')
+        {
+            i++;
+        }
+        start = i;
+        
+        /* Get the argument */
+        while (line[i] != '\n' && line[i] != '\r' && line[i] != ';' &&
+               line[i] != '\0' && line[i] != ' ')
+        { 
+            i++;
+        }
+        length = i;
 
-    /* Copy argument */
-    char arg[MAX_INSTR_LEN] = {0}; 
-    for (i = start; i < length; i++)
-    {
-        arg[i - start] = line[i];
+        /* Copy argument */
+        for (i = start; i < length; i++)
+        {
+            arg[i - start] = line[i];
+        }
+        arg[length] = '\0';
+        if (DEBUG_ASSEMBLER) printf("Arg: %s\n", arg);
     }
-    arg[length] = '\0';
-    /* printf("start: %i, length: %i, arg: %s\n", start, length, arg); */
 
     /* Parse argument */
     int arg1 = NIL;
@@ -319,6 +299,10 @@ int instr_to_code(char* line)
         arg1 = (int) strtol(arg, NULL, 10);
     }
 
+    /*
+     * Instruction comparison ladder. Could be replaced with length or
+     * first character-based comparison for speed.
+     */
     if (strcmp(instr, "NOP") == 0)
     {
         add_instr(0, arg1);
@@ -339,40 +323,45 @@ int instr_to_code(char* line)
         add_instr(3, arg1);
         return 3;
     }
-    else if (strcmp(instr, "ADD") == 0)
+    else if (strcmp(instr, "COUT") == 0)
     {
         add_instr(4, arg1);
         return 4;
     }
-    else if (strcmp(instr, "SUB") == 0)
+    else if (strcmp(instr, "ADD") == 0)
     {
         add_instr(5, arg1);
         return 5;
     }
-    else if (strcmp(instr, "MUL") == 0)
+    else if (strcmp(instr, "SUB") == 0)
     {
         add_instr(6, arg1);
         return 6;
     }
-    else if (strcmp(instr, "DIV") == 0)
+    else if (strcmp(instr, "MUL") == 0)
     {
         add_instr(7, arg1);
         return 7;
     }
-    else if (strcmp(instr, "DUP") == 0)
+    else if (strcmp(instr, "DIV") == 0)
     {
         add_instr(8, arg1);
         return 8;
     }
-    else if (strcmp(instr, "AND") == 0)
+    else if (strcmp(instr, "DUP") == 0)
     {
         add_instr(9, arg1);
         return 9;
     }
-    else if (strcmp(instr, "OR") == 0)
+    else if (strcmp(instr, "AND") == 0)
     {
         add_instr(10, arg1);
         return 10;
+    }
+    else if (strcmp(instr, "OR") == 0)
+    {
+        add_instr(11, arg1);
+        return 11;
     }
     else if (strcmp(instr, "JMP") == 0)
     {
